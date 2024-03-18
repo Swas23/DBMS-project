8ebc9f43a54d641b993fc978f161325fd55254f4
Swas23 <116055216+Swas23@users.noreply.github.com>
2024-03-18 12:49:54 +0530

    initial commit

---
+++ DBMSproj/src/main/java/com/mycompany/dbmsproj/DBMSproj.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.dbmsproj;
+
+/**
+ *
+ * @author SWASTHIK
+ */
+public class DBMSproj { 
+    
+
+    public static void main(String[] args) {
+        javax.swing.SwingUtilities.invokeLater(() -> {
+   Login l= new Login();
+   l.show();
+});
+    }
+     
+}
---
+++ DBMSproj/src/main/java/com/mycompany/dbmsproj/DefaultTableModel.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.dbmsproj;
+
+/**
+ *
+ * @author SWASTHIK
+ */
+class DefaultTableModel {
+
+    int getRowCount() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    Object getValueAt(int i, int i0) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    void addRow(Object[] object) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    void setRowCount(int i) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
